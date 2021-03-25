33f681e7d1f0cda93d2a88cad41a14182266163c
AndreiDevo <andrei@andreiai.com>
2021-03-25 20:31:03 +0000

    added basic swagger config

---
+++ src/main/java/com/andreiai/tlchallenge/configuration/SwaggerConfiguration.java
@@ -0,0 +1,46 @@
+package com.andreiai.tlchallenge.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.andreiai.tlchallenge"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiEndPointsInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        String[] supportedMethods = {};
+
+        return UiConfigurationBuilder.builder()
+                .supportedSubmitMethods(supportedMethods)
+                .build();
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder()
+                .title("TL Challenge")
+                .description("API documentation")
+                .version("1.0")
+                .build();
+    }
+}
