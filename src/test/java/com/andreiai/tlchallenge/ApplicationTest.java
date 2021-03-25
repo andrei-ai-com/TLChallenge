9ebcaa046b2e5564e97a2cd39beeb592a57eac33
AndreiDevo <andrei@andreiai.com>
2021-03-25 20:24:48 +0000

    Setup commit

---
+++ src/test/java/com/andreiai/tlchallenge/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.andreiai.tlchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void sanity_check() {
+        assertTrue(true);
+    }
+}
