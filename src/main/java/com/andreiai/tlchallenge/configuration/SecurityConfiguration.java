6bbf891c1dad513e733879dca8cd2c3d76886d15
AndreiDevo <andrei@andreiai.com>
2021-03-25 20:45:09 +0000

    Added basic security conf to enable cors and remove login form from spring boot

---
+++ src/main/java/com/andreiai/tlchallenge/configuration/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.andreiai.tlchallenge.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and().csrf().disable()
+                .cors();
+    }
+}
