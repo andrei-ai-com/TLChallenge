5e0b7cb613da97d223f785955be23d9b057b40a5
AndreiDevo <andrei@andreiai.com>
2021-03-25 23:28:11 +0000

    added curl -d url encoded instead of query param

--- src/main/java/com/andreiai/tlchallenge/provider/ShakespeareProvider.java
+++ src/main/java/com/andreiai/tlchallenge/provider/ShakespeareProvider.java
@@ -1,8 +1,15 @@
 package com.andreiai.tlchallenge.provider;
 
 import com.andreiai.tlchallenge.domain.ShakespeareResponse;
+import com.andreiai.tlchallenge.domain.exception.ShakespeareBadRequestException;
 import com.andreiai.tlchallenge.domain.exception.ShakespeareProviderException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +21,7 @@ import java.util.Objects;
 public class ShakespeareProvider {
 
     private static final String location = "https://api.funtranslations.com/translate/";
-    private static final String SHAKESPEARE_PATH = "shakespeare";
+    private static final String SHAKESPEARE_PATH = "shakespeare.json";
 
     private final RestTemplate restTemplate;
 
@@ -25,17 +32,25 @@ public class ShakespeareProvider {
     public String getShakespeareDescription(String description) {
 
         String url = UriComponentsBuilder.fromHttpUrl(location + SHAKESPEARE_PATH)
-                .queryParam("text", description)
                 .toUriString();
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> urlData = new LinkedMultiValueMap<>();
+        urlData.add("text", description);
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(urlData, headers);
+
         try {
-            return Objects.requireNonNull(restTemplate.postForObject(
-                    url,
-                    null,
-                    ShakespeareResponse.class
-            )).getContents().getTranslated();
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            throw new ShakespeareProviderException("The ShakespeareAPI is currently unavailable");
+            return Objects.requireNonNull(restTemplate.exchange(url,
+                    HttpMethod.POST,
+                    entity,
+                    ShakespeareResponse.class).getBody()).getContents().getTranslated();
+        } catch (HttpClientErrorException e) {
+            throw new ShakespeareBadRequestException(e.getMessage(), e);
+        } catch (HttpServerErrorException e) {
+            throw new ShakespeareProviderException("The ShakespeareAPI is currently unavailable", e);
         }
     }
 }
